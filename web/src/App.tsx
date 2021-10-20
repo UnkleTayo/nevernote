5fa1c01caed9db4af0b58a9a3d4ff68e0cb15b9c
Adetayo Akinsanya <akinsanyaadetayo@gmail.com>
2021-10-20 16:23:55 +0100

    Styling for components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
 import { Route, Switch } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyles } from './component/GlobalStyle'
+import Layout from './component/Layout'
 import { Home } from './pages/home'
 import { Login } from './pages/Login'
 import { SignUp } from './pages/Signup'
@@ -7,11 +9,14 @@ import { SignUp } from './pages/Signup'
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/" component={Home} />
-      </Switch>
+      <GlobalStyles />
+      <Layout>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   )
 }
--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,4 +1,7 @@
+import styled from '@emotion/styled'
 import { ChangeEvent, FormEvent, useState } from 'react'
+import { GENERICS } from '../component/GlobalStyle'
+import { Wrapper } from '../component/wrapper'
 import { useLoginMutation } from '../generated/graphql'
 
 export const Login = () => {
@@ -26,38 +29,125 @@ export const Login = () => {
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) =>
     setForm({ ...form, [e.target.name]: e.target.value })
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="login-email"></label>
-          <input
-            id="login-email"
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={form.email}
-            onChange={onChangeHandler}
+    <Wrapper center={true}>
+      <FormWrapper className="login-container">
+        <div className="left-side">
+          <img
+            src="https://businesstemplates.co.nz/wp-content/uploads/2020/12/login-concept-illustration_114360-739.jpg"
+            alt="login"
           />
         </div>
+        <div className="right-side">
+          <div>
+            <img
+              src="https://www.freeiconspng.com/uploads/evernote-icon-2.png"
+              alt=""
+            />
+            <h2>Nevernote</h2>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="login-email"></label>
+              <input
+                id="login-email"
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={form.email}
+                onChange={onChangeHandler}
+              />
+            </div>
 
-        <div>
-          <label htmlFor="login-password"></label>
-          <input
-            id="login-password"
-            type="text"
-            name="password"
-            placeholder="Password"
-            value={form.password}
-            onChange={onChangeHandler}
-          />
+            <div>
+              <label htmlFor="login-password"></label>
+              <input
+                id="login-password"
+                type="text"
+                name="password"
+                placeholder="Password"
+                value={form.password}
+                onChange={onChangeHandler}
+              />
+            </div>
+
+            {error && <div>{JSON.stringify(error)}</div>}
+
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
         </div>
-
-        {error && <div>{JSON.stringify(error)}</div>}
-
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+      </FormWrapper>
+    </Wrapper>
   )
 }
+
+const FormWrapper = styled('div')`
+  display: flex;
+  align-items: center;
+  border: ${GENERICS.border};
+  border-radius: 5px;
+  padding: 50px;
+  user-select: none;
+  gap: 20px;
+
+  > div {
+    flex: 0.5;
+  }
+
+  .left-side {
+    img {
+      width: 200px;
+    }
+  }
+
+  .right-side {
+    > div:first-of-type {
+      text-align: center;
+      img {
+        width: 50px;
+        border-radius: 10px;
+      }
+      margin-bottom: 20px;
+    }
+
+    form {
+      div {
+        margin-bottom: 10px;
+
+        input {
+          border: 2px solid gray;
+          border-radius: 5px;
+          padding: 10px 20px;
+          outline: none;
+          transition: 0.5s;
+          &:focus {
+            border-color: blue;
+          }
+        }
+
+        button {
+          border-radius: 5px;
+          width: 100%;
+          color: white;
+          background-color: ${GENERICS.primaryColor};
+          padding: 8px 20px;
+
+          &:disabled {
+            background-color: #ccc;
+          }
+        }
+        small.error-message {
+          color: red;
+        }
+      }
+
+      p {
+        font-size: 12px;
+        a {
+          color: ${GENERICS.primaryColor};
+        }
+      }
+    }
+  }
+`
